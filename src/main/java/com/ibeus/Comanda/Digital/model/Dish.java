a5eb4d71b0aaf78209a5c80b23ac65835046370c
devvictor14 <victor-marcon@hotmail.com>
2026-01-07 01:04:10 -0300

    ajustes na estrutura do projeto

---
+++ src/main/java/com/ibeus/Comanda/Digital/model/Dish.java
@@ -0,0 +1,78 @@
+package com.ibeus.Comanda.Digital.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "dishes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private String ImageUrl;
+    
+    private Integer quantity;
+    
+    public Integer getQuantity() {
+		return quantity;
+	}
+    
+    public void setQuantity(Integer quantity) {
+    	this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return ImageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        ImageUrl = imageUrl;
+    }
+}
