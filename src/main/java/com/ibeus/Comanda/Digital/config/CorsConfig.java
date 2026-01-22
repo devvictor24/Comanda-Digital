0c4d84dede1bed7f3bab9fc9c066a8782db5105f
devvictor14 <victor-marcon@hotmail.com>
2026-01-21 22:38:49 -0300

    Ajustando configurações de CORS

--- src/main/java/com/ibeus/Comanda/Digital/config/CorsConfig.java
+++ src/main/java/com/ibeus/Comanda/Digital/config/CorsConfig.java
@@ -6,13 +6,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOriginPatterns(
+                                "http://localhost:4200",
+                                "https://*.onrender.com",
+                                "https://*.vercel.app",
+                                "https://*.netlify.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
